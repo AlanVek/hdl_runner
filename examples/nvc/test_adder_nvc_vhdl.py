627858abdf115939529cff0cc3c24cf0b6ebe26c
Alan Vekselman <alanvek98@gmail.com>
2025-08-25 01:15:58 -0300

    .github/workflows/ci.yml & examples/nvc/ -- Test NVC in CI

---
+++ examples/nvc/test_adder_nvc_vhdl.py
@@ -0,0 +1,54 @@
+import cocotb
+from cocotb.triggers import ClockCycles, RisingEdge
+from cocotb.clock import Clock
+from hdl_runner.runner import run
+import os
+from random import getrandbits
+import pytest
+
+async def init_test(dut):
+    cocotb.start_soon(Clock(dut.clk, 10, 'ns').start())
+
+    dut.a.value = 0
+    dut.b.value = 0
+
+    dut.rst.value = 1
+    await ClockCycles(dut.clk, 10)
+    dut.rst.value = 0
+    await ClockCycles(dut.clk, 10)
+
+
+@cocotb.test()
+async def adder_test(dut):
+    await init_test(dut)
+
+    N_TESTS = int(os.getenv('N_TESTS', 100))
+
+    last = None
+    for i in range(N_TESTS):
+        a = getrandbits(len(dut.a))
+        b = getrandbits(len(dut.b))
+
+        dut.a.value = a
+        dut.b.value = b
+        await RisingEdge(dut.clk)
+
+        if last is not None:
+            assert last == dut.o.value.integer, f"Test failed: {last} != {dut.o.value.integer}"
+
+        last = a + b
+
+@pytest.mark.parametrize('width', [1, 2, 4, 8])
+def test(width):
+    from glob import glob
+
+    run(
+        waveform_file = 'adder_nvc.fst',
+        toplevel = 'Adder',
+        vhdl_sources = glob(os.path.join(os.path.dirname(os.path.dirname(__file__)), '*.vhd')),
+        parameters = {'size': width},
+        simulator = 'nvc',
+    )
+
+if __name__ == '__main__':
+    test(8)
